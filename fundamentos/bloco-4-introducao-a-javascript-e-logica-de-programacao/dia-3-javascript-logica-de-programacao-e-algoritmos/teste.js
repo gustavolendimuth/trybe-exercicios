d4783d72ee65b6311ebf5e027eed9929822766a0
Gustavo Lendimuth <gustavolendimuth@gmail.com>
2022-04-08 22:50:05 -0300

    remoção do teste

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/teste.js
+++
@@ -1,7 +0,0 @@
-let word = 'teste';
-let resultado="";
-for (let index = word.length; index > 0; index -= 1){
-    let indiceDaLetra = index - 1;
-    resultado += word[indiceDaLetra];
-}
-console.log(resultado);
